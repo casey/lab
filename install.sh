8332fef6dbd229c7177dd3eb6e6cb416b7fa5f50
Casey Rodarmor <casey@rodarmor.com>
2026-02-11 13:54:21 -0800

    Move install script to bin/install-hopper and update paths

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eu
-cargo build --release
-cp target/release/hopper /usr/local/bin/hopper
-cp hopper.path /etc/systemd/system/hopper.path
-cp hopper.service /etc/systemd/system/hopper.service
-systemctl daemon-reload
-systemctl enable --now hopper.path
