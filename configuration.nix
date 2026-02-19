ebf239d0289faf3d612365bbaf7ec619d268ecf2
Casey Rodarmor <casey@rodarmor.com>
2026-02-18 21:37:43 -0800

    Add Forgejo

--- configuration.nix
+++ configuration.nix
@@ -19,7 +19,7 @@
     useDHCP = true;
     firewall = {
       enable = true;
-      allowedTCPPorts = [ 22 ];
+      allowedTCPPorts = [ 22 80 443 ];
     };
   };
 
@@ -27,17 +27,78 @@
 
   time.timeZone = "America/Los_Angeles";
 
-  services.openssh = {
-    enable = true;
-    settings = {
-      PermitRootLogin = "prohibit-password";
-      PasswordAuthentication = false;
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "casey@rodarmor.com";
+  };
+
+  services = {
+    forgejo = {
+      enable = true;
+      user = "git";
+      group = "git";
+      settings = {
+        server = {
+          DOMAIN = "lab.rodarmor.com";
+          ROOT_URL = "https://lab.rodarmor.com/";
+          HTTP_ADDR = "127.0.0.1";
+          HTTP_PORT = 3000;
+        };
+        service = {
+          DISABLE_REGISTRATION = true;
+          REQUIRE_SIGNIN_VIEW = true;
+          DEFAULT_ALLOW_CREATE_ORGANIZATION = false;
+        };
+        "service.explore" = {
+          DISABLE_USERS_PAGE = true;
+          DISABLE_ORGANIZATIONS_PAGE = true;
+          DISABLE_CODE_PAGE = true;
+        };
+        repository = {
+          DISABLE_HTTP_GIT = true;
+          USE_COMPAT_SSH_URI = false;
+          ENABLE_PUSH_CREATE_USER = true;
+        };
+        session.COOKIE_SECURE = true;
+      };
+    };
+
+    nginx = {
+      enable = true;
+      virtualHosts."lab.rodarmor.com" = {
+        forceSSL = true;
+        enableACME = true;
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:3000";
+          proxyWebsockets = true;
+        };
+      };
+    };
+
+    openssh = {
+      enable = true;
+      settings = {
+        PermitRootLogin = "prohibit-password";
+        PasswordAuthentication = false;
+      };
     };
   };
 
-  users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFbSqH7DNg3/USFtrLG183EVmL7VH7v+92qMbRvlOpSy rodarmor@odin"
-  ];
+  users = {
+    users = {
+      git = {
+        home = "/var/lib/forgejo";
+        useDefaultShell = true;
+        group = "git";
+        isSystemUser = true;
+      };
+      root.openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFbSqH7DNg3/USFtrLG183EVmL7VH7v+92qMbRvlOpSy rodarmor@odin"
+      ];
+    };
+
+    groups.git = {};
+  };
 
   system.stateVersion = "26.05";
 }
