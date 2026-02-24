8fae3274b728f2a86a3cd1cc9832035573dfc28f
Casey Rodarmor <casey@rodarmor.com>
2026-02-23 18:08:39 -0800

    Improve log output

--- tests/log.rs
+++ tests/log.rs
@@ -1,18 +1,6 @@
 use super::*;
 
 #[test]
-fn log() {
-  if !std::path::Path::new("/run/systemd/journal/socket").exists() {
-    return;
-  }
-
-  Test::new()
-    .args(["log"])
-    .stdin(r#"{"foo":"bar"}"#)
-    .success();
-}
-
-#[test]
 fn non_json() {
   Test::new()
     .args(["log"])
